06ac752df199b1b2b136c2a36f2627c58f5e3261
TheNumbered <2556833@students.wits.ac.za>
2024-08-28 01:57:32 +0200

    feat(api): add Swagger API specification in backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 import cors from 'cors';
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 import userRoutes from './routes/user-route.js';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+// Read swagger.json using fs
+const swaggerPath = path.resolve('./swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', userRoutes);
 
 app.get('/', (req, res) => {
