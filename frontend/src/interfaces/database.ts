1facd27d814698661e47c46cad7889fed760b102
TheNumbered <2556833@students.wits.ac.za>
2024-09-29 22:19:09 +0200

    fix(api-intergration): remove throwing errors

--- frontend/src/interfaces/database.ts
+++ frontend/src/interfaces/database.ts
@@ -1,108 +1,107 @@
 export type Bookings = {
-    bookingId: number;
-    userId: string;
-    date: Date | string;
-    startTime: any;
-    endTime: any;
-    venueId: string;
-    status: 'pending' | 'cancelled' | 'confirmed';
-    reasonForCancellation?: string;
-    repeatFrequency: 'none' | 'daily' | 'weekly' | 'monthly';
-    eventName: string;
-    repeatUntil?: Date | string;
-}
+  bookingId: number;
+  userId: string;
+  date: Date | string;
+  startTime: any;
+  endTime: any;
+  venueId: string;
+  status: 'pending' | 'cancelled' | 'confirmed';
+  reasonForCancellation?: string;
+  repeatFrequency: 'none' | 'daily' | 'weekly' | 'monthly';
+  eventName: string;
+  repeatUntil?: Date | string;
+};
 
 export type Maintenance = {
-    issueId: number;
-    venueId?: string;
-    reportedBy?: string;
-    issueDescription: string;
-    reportedDate: Date | string;
-    resolvedDate?: Date | string;
-    resolutionLog: string;
-    imageUrl?: string;
-    status?: 'Reported';
-}
+  issueId: number;
+  venueId?: string;
+  reportedBy?: string;
+  issueDescription: string;
+  reportedDate: Date | string;
+  resolvedDate?: Date | string;
+  resolutionLog: string;
+  imageUrl?: string;
+  status?: 'Reported';
+};
 
 export type Notifications = {
-    notificationId: number;
-    userId: string;
-    message: string;
-    date: Date | string;
-    isRead: boolean;
-    route?: string;
-}
+  notificationId: number;
+  userId: string;
+  message: string;
+  date: Date | string;
+  isRead: boolean;
+  route?: string;
+};
 
 export type Rooms = {
-    roomId: string;
-    buildingId: string;
-    capacity: number;
-    type: 'LECTURE' | 'TUTORIAL' | 'LAB' | 'MEETING' | 'HALL';
-    amenities: any;
-    imageUrl: any;
-    underMaintenance: boolean;
-}
+  roomId: string;
+  buildingId: string;
+  capacity: number;
+  type: 'LECTURE' | 'TUTORIAL' | 'LAB' | 'MEETING' | 'HALL';
+  amenities: any;
+  imageUrl: any;
+  underMaintenance: boolean;
+};
 
 export type Users = {
-    userId: string;
-    fullName?: string;
-    profileUrl?: string;
-    role: 'user' | 'admin' | 'maintenance';
-    blocked: boolean;
-    createdAt: Date | string;
-}
+  userId: string;
+  fullName?: string;
+  profileUrl?: string;
+  role: 'user' | 'admin' | 'maintenance';
+  blocked: boolean;
+  createdAt: Date | string;
+};
 
 export type BookingsInput = {
-    bookingId?: number;
-    userId: string;
-    date: Date | string;
-    startTime: any;
-    endTime: any;
-    venueId: string;
-    status: 'pending' | 'cancelled' | 'confirmed';
-    reasonForCancellation?: string;
-    repeatFrequency: 'none' | 'daily' | 'weekly' | 'monthly';
-    eventName: string;
-    repeatUntil?: Date | string;
-}
+  bookingId?: number;
+  userId: string;
+  date: Date | string;
+  startTime: any;
+  endTime: any;
+  venueId: string;
+  status: 'pending' | 'cancelled' | 'confirmed';
+  reasonForCancellation?: string;
+  repeatFrequency: 'none' | 'daily' | 'weekly' | 'monthly';
+  eventName: string;
+  repeatUntil?: Date | string;
+};
 
 export type MaintenanceInput = {
-    issueId?: number;
-    venueId?: string;
-    reportedBy?: string;
-    issueDescription: string;
-    reportedDate: Date | string;
-    resolvedDate?: Date | string;
-    resolutionLog: string;
-    imageUrl?: string;
-    status?: 'Reported';
-}
+  issueId?: number;
+  venueId?: string;
+  reportedBy?: string;
+  issueDescription: string;
+  reportedDate: Date | string;
+  resolvedDate?: Date | string;
+  resolutionLog: string;
+  imageUrl?: string;
+  status?: 'Reported';
+};
 
 export type NotificationsInput = {
-    notificationId?: number;
-    userId: string;
-    message: string;
-    date: Date | string;
-    isRead: boolean;
-    route?: string;
-}
+  notificationId?: number;
+  userId: string;
+  message: string;
+  date: Date | string;
+  isRead: boolean;
+  route?: string;
+};
 
 export type RoomsInput = {
-    roomId?: string;
-    buildingId: string;
-    capacity: number;
-    type: 'LECTURE' | 'TUTORIAL' | 'LAB' | 'MEETING' | 'HALL';
-    amenities: any;
-    imageUrl: any;
-    underMaintenance: boolean;
-}
+  roomId?: string;
+  buildingId: string;
+  capacity: number;
+  type: 'LECTURE' | 'TUTORIAL' | 'LAB' | 'MEETING' | 'HALL';
+  amenities: any;
+  imageUrl: any;
+  underMaintenance: boolean;
+};
 
 export type UsersInput = {
-    userId?: string;
-    fullName?: string;
-    profileUrl?: string;
-    role: 'user' | 'admin' | 'maintenance';
-    blocked: boolean;
-    createdAt: Date | string;
-}
-
+  userId?: string;
+  fullName?: string;
+  profileUrl?: string;
+  role: 'user' | 'admin' | 'maintenance';
+  blocked: boolean;
+  createdAt: Date | string;
+};
